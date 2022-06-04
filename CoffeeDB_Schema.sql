5ed52295ab6ceb954dc6cc9eedeae2eee884956f
Julie Annis <JKAnnis@Julies-MacBook-Air.local>
2022-06-03 20:12:47 -0500

    Updated ERD and Schema and preliminary data load.

---
+++ CoffeeDB_Schema.sql
@@ -0,0 +1,66 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Species" (
+    "speciesName" varchar   NOT NULL,
+    "speciesCode" int   NOT NULL,
+    CONSTRAINT "pk_Species" PRIMARY KEY (
+        "speciesName"
+     )
+);
+
+CREATE TABLE "RobustaRatings" (
+    "recID" int   NOT NULL,
+    "species" varchar   NOT NULL,
+    "owner" varchar   NOT NULL,
+    "countryOfOrigin" varchar   NOT NULL,
+    "altitude" varchar   NOT NULL,
+    "region" varchar   NOT NULL,
+    "aroma" int   NOT NULL,
+    "flavor" int   NOT NULL,
+    "aftertaste" int   NOT NULL,
+    "acidity" int   NOT NULL,
+    "body" int   NOT NULL,
+    "balance" int   NOT NULL,
+    "uniformity" int   NOT NULL,
+    "cleanCup" int   NOT NULL,
+    "sweetness" int   NOT NULL,
+    "cupperPoints" int   NOT NULL,
+    "totalCupPoints" varchar   NOT NULL,
+    "moisture" int   NOT NULL,
+    CONSTRAINT "pk_RobustaRatings" PRIMARY KEY (
+        "recID"
+     )
+);
+
+CREATE TABLE "ArabicaRatings" (
+    "recID" int   NOT NULL,
+    "species" varchar   NOT NULL,
+    "owner" varchar   NOT NULL,
+    "countryOfOrigin" varchar   NOT NULL,
+    "altitude" varchar   NOT NULL,
+    "region" varchar   NOT NULL,
+    "aroma" int   NOT NULL,
+    "flavor" int   NOT NULL,
+    "aftertaste" int   NOT NULL,
+    "acidity" int   NOT NULL,
+    "body" int   NOT NULL,
+    "balance" int   NOT NULL,
+    "uniformity" int   NOT NULL,
+    "cleanCup" int   NOT NULL,
+    "sweetness" int   NOT NULL,
+    "cupperPoints" int   NOT NULL,
+    "totalCupPoints" varchar   NOT NULL,
+    "moisture" int   NOT NULL,
+    CONSTRAINT "pk_ArabicaRatings" PRIMARY KEY (
+        "recID"
+     )
+);
+
+ALTER TABLE "RobustaRatings" ADD CONSTRAINT "fk_RobustaRatings_species" FOREIGN KEY("species")
+REFERENCES "Species" ("speciesName");
+
+ALTER TABLE "ArabicaRatings" ADD CONSTRAINT "fk_ArabicaRatings_species" FOREIGN KEY("species")
+REFERENCES "Species" ("speciesName");
+
---
+++ config.py
@@ -0,0 +1,1 @@
+db_password = 'POST0907gres'
